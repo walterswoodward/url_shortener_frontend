a46a7972190196275000e6fdb9052e07f7a82b47
WalterSWoodward <walter.sekope.woodward@gmail.com>
2018-11-19 23:55:06 -0500

    basic functionality working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,86 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      urlInput: "",
+      newShortUrl: "",
+      youTubeLinks: [
+        "https://youtu.be/VbD_kBJc_gI?list=RDVbD_kBJc_gI",
+        "https://youtu.be/izGwDsrQ1eQ",
+        "https://youtu.be/HEXWRTEbj1I"
+      ]
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(field, value) {
+    this.setState({ [field]: value });
+  }
+
+  handleSubmit(e) {
+    
+    e.preventDefault();
+    const URL = "http://localhost:5000/";
+    // If another link was previously copied, you need to clear newShortURL
+    this.setState({
+      newShortUrl: ""
+    })
+
+    axios.get(`${URL}new/${this.state.urlInput}`).then(response => {
+      console.log(response);
+      console.log(
+        "Here is your new shortUrl LINK:",
+        `http://localhost:5000/${response.data.shortUrl}`
+      );
+      this.setState({
+        newShortUrl: <a href={`http://localhost:5000/${
+          response.data.shortUrl
+        }`} >http://localhost:5000/{
+          response.data.shortUrl
+        }</a>
+      });
+    });
+  }
+
+  handlePaste = url => {
+    this.handleChange("urlInput", url);
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h3> URL Shortener</h3>
+          <h4> Click one of the URL links below and click Submit</h4>
+          {this.state.youTubeLinks.map((item, index) => {
+            return(
+            <button
+              onClick={event => this.handlePaste(event.target.value)}
+              value={item}
+              key={index}
+            >
+              {item}
+            </button>
+          )})}
+
+          <input
+            onChange={event =>
+              this.handleChange("urlInput", event.target.value)
+            }
+            placeholder={this.state.urlInput}
+          />
+          <br />
+          <button onClick={this.handleSubmit}>Submit</button>
+          {this.state.newShortUrl === "" ? (
+            <h5> </h5>
+          ) : (
+            <div>{this.state.newShortUrl}</div>
+          )}
         </header>
       </div>
     );
