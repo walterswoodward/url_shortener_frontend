ed1c180147f9cfe0f25f5ec2ac1ba43ae48d84a9
WalterSWoodward <walter.sekope.woodward@gmail.com>
2018-11-20 14:52:04 -0500

    adds styling

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      URL: "https://z00.herokuapp.com",
       urlInput: "",
       newShortUrl: "",
       youTubeLinks: [
-        "https://youtu.be/VbD_kBJc_gI?list=RDVbD_kBJc_gI",
-        "https://youtu.be/izGwDsrQ1eQ",
-        "https://youtu.be/HEXWRTEbj1I"
+        "https://youtu.be/VbD_kBJc_gI", // "Let's Dance" by David Bowie
+        "https://youtu.be/izGwDsrQ1eQ" // "Careless Whisper" by George Michael
       ]
     };
     this.handleChange = this.handleChange.bind(this);
@@ -24,20 +24,14 @@ class App extends Component {
 
   handleSubmit(e) {
     e.preventDefault();
-    const URL = "https://z00.herokuapp.com";
-    // const URL = "http://localhost:5000";
     // If another link was previously copied, you need to clear newShortURL
     this.setState({
       newShortUrl: ""
     });
 
-    axios.get(`${URL}/new/${this.state.urlInput}`).then(response => {
+    axios.get(`${this.state.URL}/new/${this.state.urlInput}`).then(response => {
       this.setState({
-        newShortUrl: (
-          <a href={`${URL}/${response.data.shortUrl}`}>
-            {URL}/{response.data.shortUrl}
-          </a>
-        )
+        newShortUrl: response.data.shortUrl
       });
     });
   }
@@ -47,40 +41,57 @@ class App extends Component {
   };
 
   render() {
+    const renderNewUrl =
+      this.state.newShortUrl === ""? (
+        <div />
+      ) :
+      this.state.newShortUrl.includes("URL Invalid") === true? <div>Sorry! Looks like you entered an invalid URL!</div> : (
+        // <div className="newUrlContent">Your new shortened URL will appear below on submit</div>
+        <div className="newUrlContent">
+          <div className="newUrl_text">
+            Here is your new shortened URL! Go ahead and test it out!
+          </div>
+          <div className="newUrl_url">
+            {" "}
+            <a href={`${this.state.URL}/${this.state.newShortUrl}`}>
+              {this.state.URL}/{this.state.newShortUrl}
+            </a>
+          </div>
+        </div>
+      );
     return (
       <div className="App">
-        <header className="App-header">
-          <h1> URL Shortener</h1>
-          <h4> Click one of the URL links below (OR use one of your own) and click Submit</h4>
-          {this.state.youTubeLinks.map((item, index) => {
-            return (
-              <button
-                onClick={event => this.handlePaste(event.target.value)}
-                value={item}
-                key={index}
-              >
-                {item}
-              </button>
-            );
-          })}
-          <br />
-          <input
-            onChange={event =>
-              this.handleChange("urlInput", event.target.value)
-            }
-            placeholder={this.state.urlInput}
-          />
-          <br />
-          <button onClick={this.handleSubmit}>Submit</button>
-          {this.state.newShortUrl === "" ? (
-            <h5> </h5>
-          ) : (
-            <div>
-              <h5>Here is your new shortened URL! Click</h5>
-              <div>{this.state.newShortUrl}</div>
+        <div className="content_container">
+          <div className="content">
+            <div className="content_title"> URL Shortener</div>
+            <div className="content_subtitle">
+              {" "}
+              Click one of the URL links below (OR use one of your own) and
+              click Submit
             </div>
-          )}
-        </header>
+            {this.state.youTubeLinks.map((item, index) => {
+              return (
+                <button
+                  onClick={event => this.handlePaste(event.target.value)}
+                  value={item}
+                  key={index}
+                >
+                  {item}
+                </button>
+              );
+            })}
+            <br />
+            <textarea
+              onChange={event =>
+                this.handleChange("urlInput", event.target.value)
+              }
+              value={this.state.urlInput}
+            />
+            <br />
+            <button onClick={this.handleSubmit}>Submit</button>
+            {renderNewUrl}
+          </div>
+        </div>
       </div>
     );
   }
